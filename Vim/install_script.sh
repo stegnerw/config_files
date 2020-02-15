d81b80333a68938ad3c1b5cad3e5e1203bc21536
stegnerw <stegnerwayne@gmail.com>
2020-02-15 15:16:00 -0500

    Added automatic Vundle and plugin install

--- Vim/install_script.sh
+++ Vim/install_script.sh
@@ -2,3 +2,20 @@
 
 ln -s -f $(realpath vimrc) $HOME/.vimrc
 
+# Install Vundle
+
+# Check for git
+if [ -z $(which git) ]; then
+  echo "Missing dependency: git"
+  exit 1
+fi
+
+# Install Vundle and plugins
+if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
+  echo "Vundle not found. Cloning from git..."
+  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+else
+  echo "Vundle already found. Not cloning from git."
+fi
+vim +PluginInstall +qall
+
