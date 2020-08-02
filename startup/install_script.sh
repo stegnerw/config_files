7b0d68b5749e668e9f44bcc1e316877d55f2bf63
stegnerw <stegnerwayne@gmail.com>
2020-08-02 00:34:40 -0400

    Automatically get hostname

--- startup/install_script.sh
+++ startup/install_script.sh
@@ -5,10 +5,7 @@ BASE_DIR=$(realpath $(dirname "$0"))
 ALIAS_DIR=$HOME/.bash_aliases
 
 # Create necessary directories
-if [ -d $ALIAS_DIR ]; then
-  echo "WARNING: Directory already exists:"
-  echo $ALIAS_DIR
-else
+if [ ! -d $ALIAS_DIR ]; then
   mkdir $ALIAS_DIR
 fi
 
@@ -16,25 +13,33 @@ fi
 ln -sf $BASE_DIR/bashrc $HOME/.bashrc
 ln -sf $BASE_DIR/profile $HOME/.profile
 ln -sf $BASE_DIR/Xresources $HOME/.Xresources
-ln -sf $BASE_DIR/Bash_Aliases/general $ALIAS_DIR/general
 
-# Computer specific files
-echo "Which computer (for alias files)?"
-select computer in "Galapagos" "Emperor" "None"; do
-  case $computer in
-    Galapagos)
-      ln -sf $BASE_DIR/Bash_Aliases/galapagos $ALIAS_DIR/galapagos
-      break
-      ;;
-    Emperor)
-      ln -sf $BASE_DIR/Bash_Aliases/emperor $ALIAS_DIR/emperor
-      break
-      ;;
-    None)
-      break
-      ;;
-  esac
+
+
+ln -sf $BASE_DIR/bash_aliases/general $ALIAS_DIR/general
+
+# Create alias files
+for name in general $(hostname); do
+  ln -sf  $BASE_DIR/bash_aliases/$name $ALIAS_DIR/$name
 done
+
+## Computer specific files
+#echo "Which computer (for alias files)?"
+#select computer in "Galapagos" "Emperor" "None"; do
+  #case $computer in
+    #Galapagos)
+      #ln -sf $BASE_DIR/bash_aliases/galapagos $ALIAS_DIR/galapagos
+      #break
+      #;;
+    #Emperor)
+      #ln -sf $BASE_DIR/bash_aliases/emperor $ALIAS_DIR/emperor
+      #break
+      #;;
+    #None)
+      #break
+      #;;
+  #esac
+#done
 
 exit 0
 
