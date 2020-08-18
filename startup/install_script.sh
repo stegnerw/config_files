b90a888d80d2c77ddeaa69e541b8dbb04dee9d84
stegnerw <stegnerwayne@gmail.com>
2020-08-18 14:24:03 -0400

    Removed Xresources

--- startup/install_script.sh
+++ startup/install_script.sh
@@ -12,9 +12,6 @@ fi
 # Create symlinks
 ln -sf $BASE_DIR/bashrc $HOME/.bashrc
 ln -sf $BASE_DIR/profile $HOME/.profile
-ln -sf $BASE_DIR/Xresources $HOME/.Xresources
-
-
 
 ln -sf $BASE_DIR/bash_aliases/general $ALIAS_DIR/general
 
@@ -22,24 +19,6 @@ ln -sf $BASE_DIR/bash_aliases/general $ALIAS_DIR/general
 for name in general $(hostname); do
   ln -sf  $BASE_DIR/bash_aliases/$name $ALIAS_DIR/$name
 done
-
-## Computer specific files
-#echo "Which computer (for alias files)?"
-#select computer in "Galapagos" "Emperor" "None"; do
-  #case $computer in
-    #Galapagos)
-      #ln -sf $BASE_DIR/bash_aliases/galapagos $ALIAS_DIR/galapagos
-      #break
-      #;;
-    #Emperor)
-      #ln -sf $BASE_DIR/bash_aliases/emperor $ALIAS_DIR/emperor
-      #break
-      #;;
-    #None)
-      #break
-      #;;
-  #esac
-#done
 
 exit 0
 
