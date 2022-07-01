78ab543d227677b7500285e1e2094e90b6130cd7
stegnerw <wayne.stegner@protonmail.com>
2022-06-30 21:21:19 -0400

    Move git stuff to .config/git, add XDG_CONFIG_HOME

--- git/install_script.sh
+++ git/install_script.sh
@@ -2,10 +2,12 @@
 
 # Directory definitions
 BASE_DIR=$(realpath $(dirname "$0"))
+CONFIG_DIR=$XDG_CONFIG_HOME/git
+mkdir -p $CONFIG_DIR
 
 # Create symlinks
-ln -sf $BASE_DIR/gitconfig $HOME/.gitconfig
-ln -sf $BASE_DIR/gitignore $HOME/.gitignore
+ln -sf $BASE_DIR/config $CONFIG_DIR/config
+ln -sf $BASE_DIR/ignore $CONFIG_DIR/ignore
 
 exit 0
 
