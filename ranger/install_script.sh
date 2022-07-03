ac7c916dc0fc4aa1dd3e3c78be46cb6c5f92cea4
stegnerw <wayne.stegner@protonmail.com>
2022-07-03 15:30:23 -0400

    Add ranger config

---
+++ ranger/install_script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Directory definitions
+BASE_DIR=$(realpath $(dirname "$0"))
+CONFIG_DIR=$HOME/.config/ranger
+
+# Create necessary directories
+for dir in $CONFIG_DIR; do
+  mkdir -p $dir
+done
+
+# Create symlinks
+for config in $(ls $BASE_DIR); do
+  ln -sf $BASE_DIR/$config $CONFIG_DIR/$config
+done
+
+exit 0
+
