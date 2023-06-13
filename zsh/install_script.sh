228a95481ea0dfbe2d67f06162e1124e844b8686
Wayne Stegner <wayne.stegner@protonmail.com>
2023-06-12 20:16:15 -0400

    Fix oh-my-zsh plugin installation

--- zsh/install_script.sh
+++ zsh/install_script.sh
@@ -5,6 +5,7 @@ base_dir=$(realpath $(dirname "$0"))
 if [ -z $ZSH ]; then
   ZSH=$HOME/.oh-my-zsh
 fi
+rel_plugin_dir=custom/plugins
 
 # Create necessary directories
 for dir in $ZSH; do
@@ -15,7 +16,11 @@ done
 ln -sf $base_dir/zshrc $HOME/.zshrc
 ln -sf $base_dir/zprofile $HOME/.zprofile
 
-ln -sfT $base_dir/custom $ZSH/custom
+echo "Installing plugins to ${ZSH}/${rel_plugin_dir}"
+for plugin in $(ls -d $base_dir/${rel_plugin_dir}/*); do
+  echo "Installing plugin ${plugin}"
+  ln -sf ${plugin} ${ZSH}/${rel_plugin_dir}
+done
 
 exit 0
 
