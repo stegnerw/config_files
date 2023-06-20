6638dfa5e299c7b937ec7ec3cea72ce8bc3c48bb
Wayne Stegner <wayne.stegner@protonmail.com>
2023-06-19 21:47:43 -0400

    Switch startup files to systemd and xdg autostart

---
+++ autostart/install_script.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Directory definitions
+BASE_DIR=$(realpath $(dirname "$0"))
+AUTOSTART_DIR=$XDG_CONFIG_HOME/autostart
+
+# Create necessary directories
+mkdir -p $AUTOSTART_DIR
+
+ln -sf /usr/share/applications/discord.desktop "$AUTOSTART_DIR/"
+ln -sf /usr/share/applications/dropbox.desktop "$AUTOSTART_DIR/"
+ln -sf /usr/share/applications/org.flameshot.Flameshot.desktop "$AUTOSTART_DIR/"
+ln -sf /usr/share/applications/org.keepassxc.KeePassXC.desktop "$AUTOSTART_DIR/"
+ln -sf /usr/share/applications/protonmail-bridge.desktop "$AUTOSTART_DIR/"
+ln -sf /usr/share/applications/signal-desktop.desktop "$AUTOSTART_DIR/"
+ln -sf /usr/share/applications/spotify-launcher.desktop "$AUTOSTART_DIR/"
+ln -sf /usr/share/applications/thunderbird.desktop "$AUTOSTART_DIR/"
+
+for dtfile in $(ls *.desktop); do
+  ln -sf "$BASE_DIR/$dtfile" "$AUTOSTART_DIR/"
+done
+
+exit 0
